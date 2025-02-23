f126caf1f929412c2da0af3bc5b7b6048316cbb7
Gleydson Santos <gassantos.dev@gmail.com>
2025-02-22 21:16:51 -0300

    implements hook-form to validate usar data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Clients from "./pages/Clients";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/clientes" element={<Clients />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,49 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+type Inputs = {
+  user: string;
+};
+
 export default function Home() {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    localStorage.setItem("user_name", JSON.stringify(data.user));
+
+    navigate("/clientes");
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col items-center justify-center h-screen"
+    >
       <h1 className="text-4xl font-normal text-stone-950">
         Olá, seja bem-vindo!
       </h1>
-      <input
-        type="text"
-        name="user"
-        id="user"
-        placeholder="Digite seu nome:"
-        className="w-[521px] h-15 rounded-sm border-2 border-gray-200 p-4 my-5"
-      />
+      <div className="my-5 flex flex-col">
+        <input
+          id="user"
+          type="text"
+          {...register("user", { required: true })}
+          placeholder="Digite seu nome:"
+          className="w-[521px] h-15 rounded-sm border-2 border-gray-200 p-4"
+        />
+        {errors.user && <span>Este campo é obrigatório</span>}
+      </div>
       <button
-        onClick={() => console.log("click")}
+        type="submit"
         className="w-[521px] h-15 rounded-sm font-bold text-2xl text-white bg-orange-500 cursor-pointer"
       >
         Entrar
       </button>
-    </div>
+    </form>
   );
 }
